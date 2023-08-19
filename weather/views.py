0d068c30648522326864c5c76f7bb6b217c083bb
AF <100952220+ibnbayo@users.noreply.github.com>
2023-08-19 11:54:57 +0100

    Add 500 error handling

--- weather/views.py
+++ weather/views.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ForecastSerializer
-from .utils import validate_date
+
 
 import requests
 import pathlib
+import logging
+logger = logging.getLogger(__name__)
+
 
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
@@ -24,40 +27,81 @@ def ping(request):
     return Response(data)
 
 
-@api_view(['GET'])
-def forecast(request, city, at = None):
+
+def handle_error_response(e):
+  logging.error(f'Unexpected error: {e}')
   
-  if at and not validate_date(at):
-    return Response({"error": "Invalid date format"}, status=400)
+  return Response({
+    'error': 'Something went wrong', 
+    'error_code': 'internal_server_error'},
+    status=500)
 
-  api_key = settings.OPENWEATHER_API_KEY
+@api_view(['GET'])  
+def forecast(request, city):
 
-  url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+  try:
 
-  response = requests.get(url)
+    api_key = settings.OPENWEATHER_API_KEY
+    base_url = 'https://api.openweathermap.org/data/2.5/weather'
 
-  if response.status_code == 200:
+    at= request.GET.get('at')
 
-    data = response.json()
+    params = {
+            'q': city,
+            'appid': api_key,
+            'units': 'metric', 
+        }
+    
 
-    clouds = data['weather'][0]['description']
+    response = requests.get(base_url, params=params)
+  
+  except Exception as e:
+    return handle_error_response(e)
 
-    humidity = f"{data['main']['humidity']}%"
+  if response.status_code == 404:
 
-    pressure = f"{round(data['main']['pressure'] / 10)} hPa" 
+    if response.status_code == 404:
 
-    temperature = f"{round(data['main']['temp'])}°C"
+        error = response.json()['message']
+        
+        return Response({
+        'error': f"Cannot find city '{city}'", 
+        'error_code': 'city_not_found'
+        }, status=404)
 
-    forecast_data = {
-      "clouds": clouds,
-      "humidity": humidity, 
-      "pressure": pressure,
-      "temperature": temperature
-    }
 
-    serializer = ForecastSerializer(forecast_data)
-    return Response(serializer.data)
+  elif response.status_code == 200:
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+        clouds = data['weather'][0]['description']
+
+        humidity = f"{data['main']['humidity']}%"
+
+        pressure = f"{round(data['main']['pressure'] / 10)} hPa" 
+
+        temperature = f"{round(data['main']['temp'])}°C"
+
+        forecast_data = {
+        "clouds": clouds,
+        "humidity": humidity, 
+        "pressure": pressure,
+        "temperature": temperature
+        }
+
+        serializer = ForecastSerializer(forecast_data)
+        return Response(serializer.data)
 
   else:
+
     return Response({"error": "Error retrieving forecast"}, status=400)
+  
+  return Response(data)
+
+
+
+
+
 
